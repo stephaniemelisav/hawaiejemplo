94e04600de53caf47fb65cd517df52313f030205
Melisavelarde <stefydelp@gmail.com>
2021-08-26 23:25:01 -0300

    hawai

--- src/concepto programacion 1.c
+++ src/concepto programacion 1.c
@@ -12,6 +12,18 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+	//siempre que estemos en windows
+	setbuf (stdout,NULL);//para mostrar los printf
+	// tipo nombre;
+	int a; //numeros enteros
+	float f;//numeros con decimales
+	char c;//1 caracter = 1 letra = 1 simbolo
+
+	printf("Ingresame tu edad: ");
+	fflush(studin);//limpia el buffer de entrada,siempre que hay scan luego hay que poner eso
+	scanf("%d",&a);//con la d guardamos datos int, con c guardamos datos char
+
+	return
+			agus aaaaaaaaaaaaiaaaaa
+
 }
